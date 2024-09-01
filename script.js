432a0ff94f5db38432eb69301c1fcac4996dbaa1
kozzztia <kozzztia@gmail.com>
2024-09-01 17:27:35 +0300

    first test

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ console.log('go')
 // 3. Перевірка водія на наявність його ім’я у списку
 // 4. Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину. 
 
-const newCar = {
+const ford = {
     make: 'Ford',
     model: 'F-150',
     year: 2022,
@@ -16,11 +16,24 @@ const newCar = {
     costFuel: 12,
     drivers: ['Alice', 'Bob'],
     type: "pickup",
+
     description() {
-        console.log(`make: ${this.year}, model: ${this.model}, year: ${this.year}, speed: ${this.speed} km/h, tank:${this.tank}l, cost fuel: ${this.costFuel}, type:${this.type}, drivers: ${this.drivers.join(", ")}`)
+        console.log(`make: ${this.year}, model: ${this.model}, year: ${this.year}, speed: ${this.speed} km/h, tank:${this.tank}l, cost fuel: ${this.costFuel}l / 100km, type:${this.type}, drivers: ${this.drivers.join(", ")}`)
     },
     addDriver(newDriver) {
-        this.drivers = [...this.drivers, newDriver]
+        // this.drivers.push(newDriver);
+        this.drivers = [...this.drivers, newDriver];
+    },
+    findDriver(searchedDriver){
+        const isDriver = this.drivers.includes(searchedDriver);
+        console.log(isDriver?`search driver is here`:`driver ${searchedDriver} not found`)
+    },
+    wayToPoint(way){
+        const timeCount = way / (this.speed/2);
+        const fuelCount = timeCount * this.costFuel;
+        const restTimes = Math.floor(timeCount/4);
+        const fullTime = timeCount+restTimes;
+        console.log(`in way ${fullTime.toFixed(2)}, and use ${fuelCount.toFixed(2)} l fuel`)
     }
 }
 
@@ -37,19 +50,36 @@ class Car {
     }
 
     description() {
-        console.log(`make: ${this.year}, model: ${this.model}, year: ${this.year}, speed: ${this.speed} km/h, tank:${this.tank}l, cost fuel: ${this.costFuel}, type:${this.type}, drivers: ${this.drivers.join(", ")}`)
+        console.log(`make: ${this.year}, model: ${this.model}, year: ${this.year}, speed: ${this.speed} km/h, tank:${this.tank}l, cost fuel: ${this.costFuel}l / 100km, type:${this.type}, drivers: ${this.drivers.join(", ")}`)
     };
     addDriver(newDriver) {
         this.drivers = [...this.drivers, newDriver]
+    };
+    findDriver(searchedDriver){
+        const isDriver = this.drivers.includes(searchedDriver);
+        console.log(isDriver?`search driver is here`:`driver ${searchedDriver} not found`)
+    };
+    wayToPoint(way){
+        const timeCount = way / (this.speed/2);
+        const fuelCount = timeCount * this.costFuel;
+        const restTimes = Math.floor(timeCount/4);
+        const fullTime = timeCount+restTimes;
+        console.log(`in way ${fullTime.toFixed(2)}, and use ${fuelCount.toFixed(2)} l fuel`)
     }
+
 }
 
 const toyota = new Car("Toyota", "Hilux", 2020, 180, 80, 10, "John", "Doe");
 
+
 toyota.addDriver('cat')
 toyota.description();
+toyota.findDriver('red')
+toyota.wayToPoint(250)
 
 
-newCar.addDriver('vitia');
-newCar.description();
+ford.addDriver('vitia');
+ford.description();
+ford.findDriver('Bob');
+ford.wayToPoint(900)
 
